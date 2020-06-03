9b9383c6a79b8a1022c344368344f2dbbeb40044
Muslim Razi <muslimrazi@Muslims-MacBook-Pro.local>
2020-06-02 22:32:07 -0700

    get Date from dataset, format date with ISO 8601

--- StocksData.py
+++ StocksData.py
@@ -1,24 +1,28 @@
 import csv
 import collections
 import numpy as np
+from dateutil.parser import parse
 
-Prices = collections.namedtuple('Prices', field_names=['open', 'high', 'low', 'close', 'volume'])
+Prices = collections.namedtuple('Prices', field_names=['date', 'open', 'high', 'low', 'close', 'volume'])
 
 def read_csv(file_name, sep=',', filter_data=True, fix_open_price=False):
     with open(file_name, 'rt') as fd:
         reader = csv.reader(fd, delimiter=sep)
         h = next(reader)
         indices = [h.index(s) for s in ('Open','High','Low','Close','Volume')]
-        o, h, l, c, v = [], [], [], [], []
+        date_index = h.index('Date')
+        d, o, h, l, c, v = [], [], [], [], [], []
         for row in reader:
             vals = list(map(float, [row[idx].replace(',', '') for idx in indices]))
             po, ph, pl, pc, pv = vals
+            d.append(format_date(row[date_index]))
             o.append(po)
             c.append(pc)
             h.append(ph)
             l.append(pl)
             v.append(pv)
-    return Prices(open=np.array(o, dtype=np.float32),
+    return Prices(date=np.array(d, dtype=np.str),
+                  open=np.array(o, dtype=np.float32),
                   high=np.array(h, dtype=np.float32),
                   low=np.array(l, dtype=np.float32),
                   close=np.array(c, dtype=np.float32),
@@ -35,3 +39,12 @@ def prices_to_relative(prices):
     rl = (prices.low - prices.open) / prices.open
     rc = (prices.close - prices.open) / prices.open
     return Prices(open=prices.open, high=rh, low=rl, close=rc, volume=prices.volume)
+
+def format_date(date):
+    """
+    Convert a generic date into ISO 8601 date format
+    :param date: generic date
+    :return: date in ISO 8601 format
+    """
+    d = parse(date)
+    return str(d.year) + "-" + ("0" if d.month < 10 else "") + str(d.month) + "-" + ("0" if d.day < 10 else "") + str(d.day)
