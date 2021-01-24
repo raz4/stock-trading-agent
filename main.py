2105881b9828bc762fcdcb2cedd55a93a2f0f63a
Muslim Razi <razi.muslim@gmail.com>
2021-01-24 15:18:32 -0800

    replace stable-baselines with ray rllib

--- main.py
+++ main.py
@@ -2,48 +2,14 @@ from StocksEnv import StocksEnv, Actions
 import StocksData
 import CloudStorageClient
 import time
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import TRPO
+import ray
+from ray.rllib.agents.ppo import PPOTrainer
+from ray.tune import register_env
 import pandas
 import argparse
 
 # number of previous data points in time to observe
 DEFAULT_BARS_COUNT = 10
-
-def train(training_data, training_timesteps, model_file):
-    stocks_data =  StocksData.read_csv(training_data)
-    stocks_env = StocksEnv(stocks_data, bars_count=DEFAULT_BARS_COUNT, reset_on_close=False, commission_perc=0.01)
-    model = TRPO(MlpPolicy, stocks_env, verbose=1, tensorboard_log="./tensorboard/")
-    model.learn(total_timesteps=training_timesteps)
-    model.save(model_file)
-
-def test(testing_data, model_file, result):
-    model = TRPO.load(model_file)
-
-    # set testing environment
-    stock_test_data =  StocksData.read_csv(testing_data)
-    stocks_test_env = StocksEnv(stock_test_data, bars_count=10, reset_on_close=False)
-    obs = stocks_test_env.reset()
-
-    # set vars for recording results
-    result_df = pandas.DataFrame([], columns=['date', 'open', 'action', 'reward'])
-    net_reward = 0.0
-
-    while True:
-        action, _states = model.predict(obs)
-        obs, reward, done, info = stocks_test_env.step(action)
-
-        # print and record the offset, action taken, reward, opening price
-        df = pandas.DataFrame([[stock_test_data.date[int(info["offset"])], stock_test_data.open[int(info["offset"])], Actions(action).name, reward]], columns=['date', 'open', 'action', 'reward'])
-        print(df)
-        result_df = result_df.append(df, ignore_index=True)
-        net_reward += reward
-
-        # at end of episode, record results and exit
-        if done:
-            print('Net Reward: ', net_reward)
-            result_df.to_csv(result, index=False)
-            break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -51,6 +17,7 @@ if __name__ == "__main__":
     parser.add_argument('--training_timesteps', type=int, default=50000, help='total timesteps to train for')
     parser.add_argument('--testing_data', type=str, help='input testing dataset in csv format')
     parser.add_argument('--model_file', default='./models/model.zip', type=str, help='output for trained model and/or input for testing model')
+    parser.add_argument('--ckpt_path', default=None, type=str, help='checkpoint path')
     parser.add_argument('--result', default='./results/result.csv', type=str, help='output for testing results based on trained model')
     args = parser.parse_args()
 
@@ -72,11 +39,53 @@ if __name__ == "__main__":
     if result.startswith("gs://"):
         result = "./results/" + model_file.split('/')[-1].split(".zip")[0] + "_" + str(time.time()).replace('.', '') + ".csv"
 
+    ray.init()
+    config = {
+        "lr": 0.01,
+        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+        "num_gpus": 0,
+        "num_workers": 0,
+        "framework": "tf",
+        "env": "stocks_env"
+    }
+
     if args.training_data:
-        train(training_data=training_data, training_timesteps=args.training_timesteps, model_file=model_file)
+        stocks_data =  StocksData.read_csv(training_data)
+        register_env("stocks_env", lambda config: StocksEnv(stocks_data, bars_count=DEFAULT_BARS_COUNT, reset_on_close=False, commission_perc=0.01))
+        stop = {
+            # "episode_reward_mean": args.stop_reward,
+            "timesteps_total": args.training_timesteps,
+        }
+        ray.tune.run("PPO", stop=stop, config=config, verbose=1, checkpoint_freq=25000, checkpoint_at_end=True)
     
     if args.testing_data:
-        test(testing_data=testing_data, model_file=model_file, result=result)
+        stocks_data =  StocksData.read_csv(testing_data)
+        register_env("stocks_env", lambda config: StocksEnv(stocks_data, bars_count=DEFAULT_BARS_COUNT, reset_on_close=False, commission_perc=0.01))
+        agent = PPOTrainer(config, env="stocks_env")
+        agent.restore(args.ckpt_path)
+
+        stocks_test_env = StocksEnv(stocks_data, bars_count=DEFAULT_BARS_COUNT, reset_on_close=False, commission_perc=0.01)
+        obs = stocks_test_env.reset()
+        
+        # set vars for recording results
+        result_df = pandas.DataFrame([], columns=['date', 'open', 'action', 'reward'])
+        net_reward = 0.0
+
+        while True:
+            action = agent.compute_action(obs)
+            obs, reward, done, info = stocks_test_env.step(action)
+
+            # print and record the offset, action taken, reward, opening price
+            df = pandas.DataFrame([[stocks_data.date[int(info["offset"])], stocks_data.open[int(info["offset"])], Actions(action).name, reward]], columns=['date', 'open', 'action', 'reward'])
+            print(df)
+            result_df = result_df.append(df, ignore_index=True)
+            net_reward += reward
+
+            # at end of episode, record results and exit
+            if done:
+                print('Net Reward: ', net_reward)
+                result_df.to_csv(result, index=False)
+                break
 
     # upload results if GCS path was given
     if args.result.startswith("gs://"):
